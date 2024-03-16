752e074e2f8fe6ec1a8deb85bcd6d40859d7af06
SergeiMuromtsev <k29393@yandex.ru>
2024-03-16 16:32:35 +0500

    back button added via main activity

--- app/src/main/java/com/example/chat_client/MainActivity.kt
+++ app/src/main/java/com/example/chat_client/MainActivity.kt
@@ -2,7 +2,13 @@ package com.example.chat_client
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
@@ -11,10 +17,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+        //настройка панели инструментов и кнопки назад
+        //1.ссылка на контроллер навигации из хоста навигации
+        //Используем этот код, потому что вызов findNavController() из метода onCreate() активности может завершиться неудачей.
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        //конфигурация связывает панель инструментов с графом навигации
+        val builder = AppBarConfiguration.Builder(navController.graph)
+        val appBarConfiguration = builder.build()
+
+        //применяется конфигурация к панели инструментов
+        toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_toolbar, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return item.onNavDestinationSelected(navController)
+                || super.onOptionsItemSelected(item)
     }
 }
